90a82a6a18636ebedea515d45e9464d13a614927
RikuDev <riku@rikudev.com>
2023-04-20 16:48:58 +0200

    refactor, solo 1 useState recibiendo nombre de la región

--- src/components/LeagueSelector.jsx
+++ src/components/LeagueSelector.jsx
@@ -4,146 +4,38 @@ import Pokedex from '../hooks/Pokedex';
 
 
 const LeagueSelector = () => {
-    const [KantoVisible, setKantoVisible] = useState(false);
-    const [JhotoVisible, setJhotoVisible] = useState(false);
-    const [HoennVisible, setHoennVisible] = useState(false);
-    const [SinnohVisible, setSinnohVisible] = useState(false);
-    const [TeseliaVisible, setTeseliaVisible] = useState(false);
-    const [KalosVisible, setKalosVisible] = useState(false);
-    const [AlolaVisible, setAlolaVisible] = useState(false);
-    const [GalarVisible, setGalarVisible] = useState(false);
-    const [OtherVisible, setOtherVisible] = useState(false);
+    const [visible, setVisible] = useState('');
 
-    function handleClickKanto() {
-        setKantoVisible(true);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-        
-    }
-    function handleClickJhoto() {
-        setJhotoVisible(true);
-        setKantoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickHoenn() {
-        setHoennVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickSinnoh() {
-        setSinnohVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickTeselia() {
-        setTeseliaVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickKalos() {
-        setKalosVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickAlola() {
-        setAlolaVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setGalarVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickGalar() {
-        setGalarVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setOtherVisible(false);
-    }
-    function handleClickOther() {
-        setOtherVisible(true);
-        setKantoVisible(false);
-        setJhotoVisible(false);
-        setHoennVisible(false);
-        setSinnohVisible(false);
-        setTeseliaVisible(false);
-        setKalosVisible(false);
-        setAlolaVisible(false);
-        setGalarVisible(false);
+    const handleClick = (name) => {
+        setVisible(name);
     }
 
     return (
         <div className='selector-container'>
             <div className='buttons'>
-                <button className='kantoBtn' onClick={handleClickKanto}>KANTO</button>
-                <button className='jhotoBtn' onClick={handleClickJhoto}>JHOTO</button>
-                <button className='hoennBtn' onClick={handleClickHoenn}>HOENN</button>
-                <button className='sinnohBtn' onClick={handleClickSinnoh}>SINNOH</button>
-                <button className='teseliaBtn' onClick={handleClickTeselia}>TESELIA</button>
-                <button className='kalosBtn' onClick={handleClickKalos}>KALOS</button>
-                <button className='alolaBtn' onClick={handleClickAlola}>ALOLA</button>
-                <button className='galarBtn' onClick={handleClickGalar}>GALAR</button>
-                <button className='othersBtn' onClick={handleClickOther}>OTHERS</button>
+                <button className='kantoBtn'  onClick={() => handleClick('kanto')}>KANTO</button>
+                <button className='jhotoBtn'  onClick={() => handleClick('jhoto')}>JHOTO</button>
+                <button className='hoennBtn'  onClick={() => handleClick('hoenn')}>HOENN</button>
+                <button className='sinnohBtn'  onClick={() => handleClick('sinnoh')}>SINNOH</button>
+                <button className='teseliaBtn'  onClick={() => handleClick('teselia')}>TESELIA</button>
+                <button className='kalosBtn'  onClick={() => handleClick('kalos')}>KALOS</button>
+                <button className='alolaBtn'  onClick={() => handleClick('alola')}>ALOLA</button>
+                <button className='galarBtn'  onClick={() => handleClick('galar')}>GALAR</button>
+                <button className='othersBtn'  onClick={() => handleClick('others')}>OTHERS</button>
             </div>
-
             <div className='pokemons'>
-                {KantoVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=151"/>}
-                {JhotoVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=100&offset=151"/>}
-                {HoennVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=135&offset=251"/>}
-                {SinnohVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=107&offset=386"/>}
-                {TeseliaVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=156&offset=493"/>}
-                {KalosVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=71&offset=649"/>}
-                {AlolaVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=81&offset=721"/>}
-                {GalarVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=96&offset=802"/>}
-                {OtherVisible && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=0&offset=898"/>}
+                {visible === 'kanto' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=151"/>}
+                {visible === 'jhoto' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=100&offset=151"/>}
+                {visible === 'hoenn' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=135&offset=251"/>}
+                {visible === 'sinnoh' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=107&offset=386"/>}
+                {visible === 'teselia' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=156&offset=493"/>}
+                {visible === 'kalos' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=71&offset=649"/>}
+                {visible === 'alola' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=81&offset=721"/>}
+                {visible === 'galar' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=96&offset=802"/>}
+                {visible === 'others' && <Pokedex url="https://pokeapi.co/api/v2/pokemon?limit=0&offset=898"/>}
             </div>
-
-
         </div>
     );
-}
+    }
 
 export default LeagueSelector;
